fcd2b3d10c31213002cbd10f6df1841642cdf7a4
Aarthi1693 <Aarthi1693_aarthiv1693@gmail.com>
2026-08-12 21:41:10 +0530

    Fix KYC upload directory creation

--- src/kyc/multer.config.ts
+++ src/kyc/multer.config.ts
@@ -1,36 +1,40 @@
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export const multerOptions = {
   storage: diskStorage({
     destination: (req, file, cb) => {
       const type = req.params.type;
 
-      let folder = './src/kyc/uploads';
+      let folder = path.join(process.cwd(), 'src', 'kyc', 'uploads');
 
       switch (type) {
         case 'aadhaar':
-          folder += '/aadhaar';
+          folder = path.join(folder, 'aadhaar');
           break;
 
         case 'pan':
-          folder += '/pan';
+          folder = path.join(folder, 'pan');
           break;
 
         case 'selfie':
-          folder += '/selfie';
+          folder = path.join(folder, 'selfie');
           break;
 
         default:
-          folder += '/others';
+          folder = path.join(folder, 'others');
       }
+
+      // Create folder if it doesn't exist
+      fs.mkdirSync(folder, { recursive: true });
 
       cb(null, folder);
     },
 
     filename: (req, file, cb) => {
       const unique = Date.now() + '-' + Math.round(Math.random() * 1e9);
-
       cb(null, unique + extname(file.originalname));
     },
   }),
